8976580f0dae781665bebef126a5d4bbebc6acd5
rknm-cell <racknahm@gmail.com>
2025-06-23 16:13:24 -0400

    initiated sort react page

--- src/app/sort/page.tsx
+++ src/app/sort/page.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function Quicksort() {
-  return (
-    <div>
-      <div>
+  const [step, setStep] = useState(0);
+  const sortArray: number[] = [1, 9, 2, 8, 3, 7, 4, 6, 5];
+  function handleSort(array: number[]): number[] {
+    return array;
+  }
 
+  function handleRender(array: number[]) {
+    return array.map((n, index) => <div className="p-1 border-1" key={index}>{n}, </div>);
+  }
+  return (
+    <div className="flex max-w-dvh flex-col justify-center align-middle">
+      <h1 className="text-xl font-bold">Merge Sort</h1>
+      <div>
+        <div className="flex flex-row">
+
+        {handleRender(sortArray)}
+        </div>
         
+        <div>
+          <button
+            onClick={() => {
+              setStep(step - 1);
+            }}
+            className="w-20 border"
+          >
+            {"<"}
+          </button>
+          <button
+            onClick={() => {
+              setStep(step + 1);
+            }}
+            className="w-20 border"
+          >
+            {">"}{" "}
+          </button>
+        </div>
       </div>
-      Quicksort
     </div>
   );
 }
