ee98bf468967e179086bfd36073990a558e00bee
rknm-cell <racknahm@gmail.com>
2025-06-23 15:44:52 -0400

    completed mergesort

--- mergesort.ts
+++ mergesort.ts
@@ -1,29 +1,28 @@
 const arraynums: number[] = [1, 9, 2, 8, 3, 7, 4, 6, 5];
 
 function merge(leftArray: number[], rightArray: number[]): number[] {
-  const mergedArray = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
-    if (leftArray[leftIndex] < rightArray[rightIndex]) {
-      mergedArray.push(leftArray[leftIndex]);
-      leftIndex++;
-    } else if (leftArray[leftIndex] > rightArray[rightIndex]) {
-      mergedArray.push(rightArray[rightIndex]);
-      rightIndex++;
-    } else {
-      mergedArray.push(rightArray[rightIndex]);
-      mergedArray.push(leftArray[leftIndex]);
+    const mergedArray: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+        if (leftArray[leftIndex] < rightArray[rightIndex]){
+            mergedArray.push(leftArray[leftIndex])
+            leftIndex++
+        }
+        else if (leftArray[leftIndex] > rightArray[rightIndex]){
+            mergedArray.push(rightArray[rightIndex])
+            rightIndex++
+        }
+        
     }
-  }
-  while (leftArray.length < leftIndex){
-    mergedArray.push(leftArray[leftIndex])
-  }
-  while (rightArray.length < rightIndex){
-    mergedArray.push(rightArray[rightIndex])
-  }
-
-  console.log("MergedArray: ", mergedArray);
+    while (leftIndex < leftArray.length){
+        mergedArray.push(leftArray[leftIndex])
+        leftIndex++
+    }
+    while (rightIndex < rightArray.length){
+        mergedArray.push(rightArray[rightIndex])
+        rightIndex++
+    }
   return mergedArray;
 }
 function mergeSort(array: number[]): number[] {
@@ -32,9 +31,9 @@ function mergeSort(array: number[]): number[] {
   }
   const middle = array.length / 2;
   const leftArray = array.slice(0, middle);
-  console.log("left: ",leftArray)
+
   const rightArray = array.slice(middle);
-  console.log("right: ",rightArray)
+  
   const sortedLeft = mergeSort(leftArray);
   const sortedRight = mergeSort(rightArray);
 
