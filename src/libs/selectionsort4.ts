22cba30fbd0a6255b503c0ffcfed8b509d570e5c
rknm-cell <racknahm@gmail.com>
2025-06-27 10:48:49 -0400

    selectionsort and quicksort drills

---
+++ src/libs/selectionsort4.ts
@@ -0,0 +1,15 @@
+import { arraynums, arraynums2, arraynums3 } from "./constants";
+
+
+function selectionSort(arr: number[]):number[]{
+    return arr
+}
+
+
+
+console.log(arraynums)
+console.log(selectionSort(arraynums))
+console.log(arraynums2)
+console.log(selectionSort(arraynums2))
+console.log(arraynums3)
+console.log(selectionSort(arraynums3))
